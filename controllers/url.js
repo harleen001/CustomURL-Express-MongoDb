f049969c7c9173f6eeb5ef6a61da90df5ff0e471
harleen <harleen.johal31@gmail.com>
2024-05-12 15:15:58 +0530

    minor

--- controllers/url.js
+++ controllers/url.js
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid';
 import { create } from '../models/url';
 async function HandlegenerateNewShortURL(req,res){
     const body = req.body;
-
+S
     if(!body.url) return res.status(400).json({error: 'url is required'}
     )
 const shortID= nanoid(8);
