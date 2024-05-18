82c0980138f7256e3ba9898ad0a76dab2be8e5e6
harleen <harleen.johal31@gmail.com>
2024-05-18 18:11:40 +0530

    controllers-updated

--- controllers/url.js
+++ controllers/url.js
@@ -1,21 +1,50 @@
-import { nanoid } from 'nanoid';
-import { create } from '../models/url';
-async function HandlegenerateNewShortURL(req,res){
-    const body = req.body;
-S
-    if(!body.url) return res.status(400).json({error: 'url is required'}
-    )
-const shortID= nanoid(8);
-await create({
+const shortid = require("shortid");
+const URL = require("./../models/url");
 
+// Generate URL
+exports.generateNewShortURL = async (req, res) => {
+  const body = req.body;
+  const shortID = shortid.generate(); // Correct function to generate short ID
 
+  if (!body.url)
+    return res.status(400).json({
+      error: "URL is required",
+    });
 
-    shortId:shortID,
-    redirectUrl: body.url,
-    visitHistory: [],
-});
-return res.json({id:shortID});
-}
-export default{
-    HandlegenerateNewShortURL,
-}
+  try {
+    await URL.create({
+      shortID: shortID,
+      redirectURL: body.url,
+      visitHistory: [],
+    });
+
+    res.status(200).json({
+      id: shortID,
+    });
+  } catch (error) {
+    console.error("Error generating short URL:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// Get number of times URL clicked
+exports.getAnalytics = async (req, res) => {
+  const shortID = req.params.shortID;
+
+  try {
+    const result = await URL.findOne({ shortID });
+
+    if (!result)
+      return res.status(404).json({
+        error: "URL not found",
+      });
+
+    return res.json({
+      totalClicks: result.visitHistory.length,
+      analytics: result.visitHistory,
+    });
+  } catch (error) {
+    console.error("Error fetching analytics:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
