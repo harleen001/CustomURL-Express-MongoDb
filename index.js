d0eb113bd9972f6925cf2e833b622571892f89fe
harleen <harleen.johal31@gmail.com>
2024-05-18 18:10:00 +0530

    main-fixed

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
-import express from 'express';
+const { connectToDatabase } = require("./connect");
+const express = require("express");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
 const app = express();
-const { connectMongoDB } = require('./connect.js');
-import urlRoute from './routes/url.js';
-const PORT=8001;
+const port = 8001;
+
+connectToDatabase(
+  "mongodb://127.0.0.1:27017/url"
+).then(() => console.log("Database connected"));
+
+app.use(express.json());
+
+app.get("/:shortID", async (req, res) => {
+  const shortID = req.params.shortID;
+
+  const entry = await URL.findOneAndUpdate(
+    { shortID },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    },
+    { new: true } // Return the updated document
+  );
+
+  if (entry && entry.redirectURL) {
+    res.redirect(entry.redirectURL);
+  } else {
+    // Handle the case where entry is null or entry.redirectURL is not available
+    // You could send an error response or redirect to a default URL
+    res.status(404).send("URL not found");
+  }
+});
 
 
+app.use("/url", urlRoute);
 
-connectMongoDB("mongodb://127.0.0.1:27017/short-url")
-.then(console.log("MongoDB Connected"));
-
-
-app.use("/url",urlRoute);
-
-
-app.listen(PORT,()=> console.log(`Server started at PORT : ${PORT}`))
+app.listen(port, () => {
+  console.log(`Server started at PORT: ${port}.`);
+});
