acd0d58d9dc49bc6c78896c7a2cf8d7735f1527b
harleen <harleen.johal31@gmail.com>
2024-05-18 18:12:51 +0530

    models-done

--- models/url.js
+++ models/url.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose');
-const urlSchema = new mongoose.Schema({
+const mongoose = require("mongoose");
 
-shortId:{
-    type: String,
-    required: true,
-    unique: true,
-},
-redirectUrl:{
-    type: String,
-    required: true,
-},
-visitHistory:[{timestamp:{type:Number}}],},
-{timestamps:true}
+const urlSchema = new mongoose.Schema(
+  {
+    shortID: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    redirectURL: {
+      type: String,
+      required: true,
+    },
+
+    visitHistory: [{ timestamp: { type: Number } }],
+  },
+  { timestamps: true }
 );
 
-const URL=mongoose.model('url',urlSchema);
+const URL = mongoose.model("URL", urlSchema); // Capitalize model name
 
-module.exports=URL;
-
+module.exports = URL;
