27aca343dbea016b5beea170381b651d5c3912d3
harleen <harleen.johal31@gmail.com>
2024-05-18 18:10:59 +0530

    url-fixed

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const { HandlegenerateNewShortURL} = require('../controllers/url').default;
+const express = require("express");
+const urlController = require("../controllers/url"); // Correct controller import
 const router = express.Router();
 
+// URL shortener router
+router.post("/", urlController.generateNewShortURL);
 
-router.post('/',HandlegenerateNewShortURL);
-module.exports=router;
+// Get the number of times link used
+router.get("/analytics/:shortID", urlController.getAnalytics);
+
+module.exports = router;
